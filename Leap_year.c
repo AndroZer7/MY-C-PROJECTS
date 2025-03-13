2926934d5c17e454c4eac2e742065f4ea4a8ae77
AndroZer7 <itsdebatra@gnail.com>
2025-03-13 12:43:48 +0530

    Update

--- Leap_year.c
+++ Leap_year.c
@@ -24,20 +24,5 @@ int main()
 // Function to check if a year is a leap year or not.
 int isLeapYear(int year)
 {
-    if (year % 400 == 0)
-    {
-        return 1;
-    }
-    else if (year % 100 == 0)
-    {
-        return 0;
-    }
-    else if (year % 4 == 0)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
+    return (year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0));
 }
